c1c0dbc686f0efc188d1523c3920a99b45c9a579
Michishio <113250941+AwantikaAnkushe@users.noreply.github.com>
2024-05-03 22:59:59 +0530

    Update Source_Code.java

--- Source_Code.java
+++ Source_Code.java
@@ -45,10 +45,10 @@ public class Game extends JPanel implements ActionListener {
     public void draw(Graphics g) {
         if (running) {
             /*
-             * for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
-             * g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
-             * g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
-             * }
+              for(int i=0;i<SCREEN_HEIGHT/UNIT_SIZE;i++){
+              g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+              g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
+             }
              */
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
